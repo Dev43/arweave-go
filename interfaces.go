25f8af67e4d9a9c38b5b4c9113f3804264d06cb9
Dev43 <patrick.guay43@gmail.com>
2019-07-19 17:23:29 +0700

    fix imports

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,11 @@
 package arweave
 
+import (
+	"context"
+
+	"github.com/Dev43/arweave-go/tx"
+	"github.com/Dev43/arweave-go/wallet"
+)
 
 type Transactor interface {
 	CreateTransaction(w *wallet.Wallet, amount string, data []byte, target string) (*tx.Transaction, error)
