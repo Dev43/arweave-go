72a8f735d1d81e494b1433f2ccc67fcb11fee304
Dev43 <patrick.guay43@gmail.com>
2019-07-22 17:00:20 +0700

    fix interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,27 +1,7 @@
 // Package arweave defines interfaces for interacting with the Arweave Blockchain.
 package arweave
 
-import (
-	"context"
-
-	"github.com/Dev43/arweave-go/tx"
-	"github.com/Dev43/arweave-go/wallet"
-)
-
-type TransactionHandler interface {
-	CreateTransaction(w *wallet.Wallet, amount string, data []byte, target string) (*tx.Transaction, error)
-	WaitMined(ctx context.Context, tx *tx.Transaction) (*tx.JSONTransaction, error)
-	SendTransaction(tx *tx.Transaction) (string, error)
-}
-
-type TransactionCreator interface {
-	CreateTransaction(w *wallet.Wallet, amount string, data []byte, target string) (*tx.Transaction, error)
-}
-
-type TransactionSender interface {
-	SendTransaction(tx *tx.Transaction) (string, error)
-}
-
+// WalletSigner is the interface needed to be able to sign an arweave transaction
 type WalletSigner interface {
 	Sign(msg []byte) ([]byte, error)
 	Verify(msg []byte, sig []byte) error
